09e16a46aca270965cf8c24bde25355c46185fc0
Mallika Mathur <mkmathur@cs.uw.edu>
2014-06-21 13:46:17 -0700

    added books page stub, before block to check for access token

--- app.rb
+++ app.rb
@@ -1,8 +1,7 @@
 require 'rubygems'
 require 'sinatra'
 require 'Haml'
-require './goodreads_module'
-include Goodreads
+require './goodreads'
 
 def oauth_consumer
 	OAuth::Consumer.new(KEY, SECRET, :site =>  "http://www.goodreads.com")
@@ -10,9 +9,18 @@ end
 
 configure do
 	enable :sessions
-	# developer key
 	KEY = "Yf6QamFRu4dL3dhbz237Sw"
 	SECRET = "RrkCTmZIS8zuNsjuNf412vaZmlHHJ17W6pRRVsr4"
+end
+
+before do
+	if session[:access_token]
+		gr_oauth = Goodreads::Oauth.new(KEY, SECRET)
+		gr_oauth.authorize_from_access(session[:access_token], session[:access_token_secret])
+		@goodreads = Goodreads::Base.new(gr_oauth)
+	else
+		@goodreads = nil
+	end
 end
 
 get '/' do
@@ -21,4 +29,9 @@ get '/' do
 	session[:request_token_secret] = request_token.secret 
 	@url = request_token.authorize_url
 	haml :index
+end
+
+get '/books' do
+	redirect '/' unless @goodreads
+	haml :books
 end
