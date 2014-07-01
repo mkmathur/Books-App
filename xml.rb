e02f8057449b8cdb068790d8a79ee611ac541374
Mallika Mathur <mkmathur@cs.uw.edu>
2014-06-30 17:14:49 -0700

    added command line argument for shelf name

--- xml.rb
+++ xml.rb
@@ -1,13 +1,17 @@
+require './goodreads'
+
 KEY = "Yf6QamFRu4dL3dhbz237Sw"
 SECRET = "RrkCTmZIS8zuNsjuNf412vaZmlHHJ17W6pRRVsr4"
 
 MY_ACCESS_TOKEN = "OXjo19O4gThQMIsywElUvw"
 MY_ACCESS_TOKEN_SECRET = "7lJhrwVad6Txy3yC1ws2y0J5tphC9MPaCJ9jaGFro"
 
-def setupTestShelf
+def getXML(shelf)
 	oauth = Goodreads::Oauth.new(KEY, SECRET)
 	oauth.authorize_from_access(MY_ACCESS_TOKEN, MY_ACCESS_TOKEN_SECRET)
 
 	gr = Goodreads::Base.new(oauth)
-	gr.shelf('test', true)
+	gr.shelf(shelf, true)
 end
+
+getXML(ARGV[0])
